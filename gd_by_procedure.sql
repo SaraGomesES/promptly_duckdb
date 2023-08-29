9caf69817ce94cd23a7f7bccf1dd2ac877d2fd0a
Sara Gomes <sara.gomes.es@gmail.com>
2023-08-29 10:44:19 -0300

    Merge branch gender-distribution

---
+++ gd_by_procedure.sql
@@ -0,0 +1,12 @@
+-- gender distribution by procedures:
+
+select
+description,
+count(*) as total_patients,
+sum(case when gender = 'M' then 1 else 0 end) as total_male,
+sum(case when gender = 'F' then 1 else 0 end) as total_female,
+round((sum(case when gender = 'M' then 1 else 0 end) * 100 / count(*)), 2) as male_proportion,
+round((sum(case when gender = 'F' then 1 else 0 end) * 100 / count(*)), 2) as female_proportion
+from procedures_patient
+group by description
+order by description;
